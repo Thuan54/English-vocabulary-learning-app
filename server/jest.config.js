bf6fdd60e637ca290fe2f5fdf212f1b412f05e54
Thuan <24120459@gmail.student.hcmus.edu.vn>
2026-04-12 23:12:29 +0700

    test: add ci workflow

--- server/jest.config.js
+++ server/jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   clearMocks: true,
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+    },
+  },
 };
