b399e2ca981449ab43b10a64ba00fa769298e8b1
Thuan <24120459@gmail.student.hcmus.edu.vn>
2026-04-26 11:45:36 +0700

    feat: update route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import { Review } from "./pages/Review";
+import { Dashboard } from "./pages/Dashboard";
+import { VocabularySearch } from "./pages/VocabularySearch";
+import { RelatedWordsPage } from "./pages/VocabularyMananger";
+import { StudySessionPage } from "./pages/StudySessionPage";
+import { Layout } from "./components/Layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    Component: Layout,
     children: [
+      { index: true, Component: Dashboard },
       { path: "review", Component: Review },
+      { path: "search", Component: VocabularySearch },
+      { path: "relate", Component: RelatedWordsPage },
+      { path: "study", Component: StudySessionPage },
     ],
   },
 ]);
